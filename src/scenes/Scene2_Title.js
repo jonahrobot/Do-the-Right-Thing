d34b7b68b273ef94b5dd68010fbe7f2d8cfe3804
Jonah Ryan <jopryan@ucsc.edu>
2023-06-10 17:54:24 -0700

    Added scene 2

--- src/scenes/Scene2_Title.js
+++ src/scenes/Scene2_Title.js
@@ -28,9 +28,9 @@ class Scene2_Title extends Phaser.Scene{
         this.add.image(w/4.5,h/2,'movie_box').setOrigin(0.5).setScale(0.35);
 
         // Create menu text
-        this.title = this.add.text(2*(w/4.5), h/4, 'Do the Right Thing', menuConfig).setOrigin(0);
+        this.title = this.add.text(2*(w/4.5), h/4, 'Scene 2', menuConfig).setOrigin(0);
         menuConfig.fontSize = '20px'
-        this.main = this.add.text(2*(w/4.5), h/2.5, 'A Spike Lee classic where misunderstandings and racist ideologies cross to create a tragedy that affects all involved.', menuConfig).setOrigin(0);
+        this.main = this.add.text(2*(w/4.5), h/2.5, 'This scene depicts a moment in the movie where a rich Italian tourist gets into a argument with local kids. It ends in his car being sprayed down with water damaging its finish.', menuConfig).setOrigin(0);
         menuConfig.fontSize = '28px'
         this.add.text(2*(w/4.5), h-100, '-> to continue', menuConfig).setOrigin(0);
 
@@ -39,14 +39,7 @@ class Scene2_Title extends Phaser.Scene{
     // Check for right key input
     update(){
         if (Phaser.Input.Keyboard.JustDown(this.keyRIGHT)) {
-            if(this.index == 0){
-                this.title.text = "Scene 1"
-                this.main.text = "You play as Sal, a Italian American Restaurant owner setting up his wall of Fame. You will notice a lack of room for non Italian Americans, which builds the center conflict of the story."
-            }
-            if(this.index == 1){
-                this.scene.start('playScene');
-            }
-            this.index += 1;
+            this.scene.start('playScene');
         }
     }
 }
